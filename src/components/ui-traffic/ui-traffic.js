be3b251755687cff777fb4ccd8250d184ad8aafc
Jacques W <jacquesn4444@gmail.com>
2016-04-10 23:08:55 +0200

    initial

---
+++ src/components/ui-traffic/ui-traffic.js
@@ -0,0 +1,20 @@
+/* global JustGage */
+/* global angular */
+angular.module('ui').directive('uiTraffic', [ '$timeout', '$interpolate',
+    function ($timeout, $interpolate) {
+        return {
+			restrict: 'E',
+			replace: true,
+			templateUrl: 'components/ui-traffic/ui-traffic.html',
+			link: function(scope, element, attrs) {
+				$timeout(function() {
+				
+					scope.$watch('me.item.value', function (newValue) {
+						newValue = newValue || 'green';
+						scope.me.item.red = scope.me.item.green = scope.me.item.orange = false;
+						scope.me.item[newValue] = true;
+					});
+				}, 0);
+			}
+		}
+    }]);
